d339c9aaeab766aac4892c53656c0a210f160136
callumjhb <callumjhbenfell@gmail.com>
2024-06-08 12:11:01 +1200

    Sketch.js changes

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,35 +18,48 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  background(0, 0, 0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<4000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
-    if(mask[0] > 128) {
-      let pointSize = 10;
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+  let num_lines_to_draw = 40;
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+    for(let i=0; i<1920; i++) {
+
+      colorMode(RGB);
+      let pix = sourceImg.get(i, j);
+      let col = color(pix);
+      let mask = maskImg.get(i, j);
+
+      colorMode(HSB, 360, 100, 100);
+      let h = hue(col);
+      let b = brightness(col);
+
+      if(mask[0] > 128) {
+        let new_brt = map(b, 0, 100, 100, 0);
+        let new_col = color(h, 0, new_brt);
+        set(i, j, new_col);
+      }
+
+      else {
+        let new_col = color(h, 0, b);
+        set(i, j, new_col);
+      }
     }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  renderCounter = renderCounter + num_lines_to_draw;
+  updatePixels();
+  // print(renderCounter);
+  if(renderCounter > 1080) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
     // saveArtworkImage(outputFile);
   }
 }
+
 
 function keyTyped() {
   if (key == '!') {
